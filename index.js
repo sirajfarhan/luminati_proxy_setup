7b19479e41a44b9ec4fbf1fd9098f86d282f9ad0
Siraj Farhan <hello@getjamb.com>
2020-03-08 15:39:26 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const request = require('request-promise');
+const delay = require('delay');
+
+const options = {
+  method: 'POST',
+  uri: 'http://127.0.0.1:22999/api/proxies',
+  json: true,
+  headers: {
+    'Connection': 'keep-alive',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36',
+    'Content-Type': 'application/json'
+  },
+  body: {
+    proxy: {
+      preset: "session_long",
+      zone: "",
+      proxy_type:"persist",
+      pool_size:1,
+      session:"",
+      port:24000
+    }
+  }
+};
+
+async function main() {
+    await request(options);
+    await delay(5000);
+}
+
+main()
